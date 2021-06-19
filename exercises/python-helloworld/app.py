62ec0832dda7a3e5287a5a42edad6ef352323665
Filipe Bezerra <filipebzerra@gmail.com>
2021-06-19 14:17:38 -0300

    Gives dynamic behavior to health check and metrics endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 from flask import make_response
+from random import randint
 
 @app.route("/")
 def hello():
@@ -8,11 +9,17 @@ def hello():
 
 @app.route("/status")
 def healthz():
-    return make_response({ "result": "OK - healthy"})
+    status = "OK - healthy" if randint(1, 9) % 2 == 0 else "401 - error"
+    return make_response({ "result": status})
 
 @app.route("/metrics")
 def metrics():
-    return { "user_count": 2000, "user_count_active": 100}
+    user_count = randint(1, 9999)
+    user_count_active = randint(1, user_count)
+    return {
+        "user_count": user_count,
+        "user_count_active": user_count_active
+    }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
