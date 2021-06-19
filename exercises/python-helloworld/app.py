f4518f30007de32d3ea35573b7fe64023ff01847
Filipe Bezerra <filipebzerra@gmail.com>
2021-06-19 14:15:15 -0300

    Static health check and metrics endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
+from flask import make_response
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def healthz():
+    return make_response({ "result": "OK - healthy"})
+
+@app.route("/metrics")
+def metrics():
+    return { "user_count": 2000, "user_count_active": 100}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
